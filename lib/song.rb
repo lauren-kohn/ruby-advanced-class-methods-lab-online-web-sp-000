cf5abd56456ed87bc81017681723006d6b278188
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-07-30 20:22:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,15 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
   def self.all
     @@all
   end
@@ -9,5 +17,39 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.new_by_name(name)
+    song = Song.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end 
+  
+  def self.find_by_name(name)
+    @@all.detect do |song|
+      song.name == name 
+    end
+  end
+  
+  def self.find_or_create_by_name(name)
+    match = self.find_by_name(name)
+    if match
+      match
+    else 
+      self.create_by_name(name)
+    end
+  end 
+  
+  def self.alphabetical
+    sorted = @@all.sort_by do |a, b|
+      a <=> b 
+    end
+    sorted
+  end
+  
 end
